181f8696d647d73baf0245ebf1214909410a4ec1
Ofx Steve <stephemutiso19@gmail.com>
2026-04-07 15:48:16 +0300

    Add global footer signature

--- srms/script/js/main.js
+++ srms/script/js/main.js
@@ -20,4 +20,18 @@
 		$(this).parent().toggleClass('is-expanded');
 	});
 
+	// Global footer
+	if (!document.getElementById('appFooter')) {
+		var footer = document.createElement('footer');
+		footer.id = 'appFooter';
+		footer.className = 'app-footer';
+		footer.textContent = '@2026 powered by ofx_steve';
+		var content = document.querySelector('.app-content');
+		if (content && content.parentNode) {
+			content.parentNode.appendChild(footer);
+		} else {
+			document.body.appendChild(footer);
+		}
+	}
+
 })();
