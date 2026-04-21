7857dacf094c41e844dad8cf8e8125b6685f75a8
Ofx Steve <stephemutiso19@gmail.com>
2026-04-21 15:15:26 +0300

    Group teacher modules in sidebar

--- srms/script/teacher/partials/sidebar.php
+++ srms/script/teacher/partials/sidebar.php
@@ -36,6 +36,21 @@ function teacher_menu_module_active(array $module)
 }
 
 $allocatedSidebarModules = app_current_user_allocated_portal_modules('teacher');
+$allocatedSidebarModulesByKey = [];
+foreach ($allocatedSidebarModules as $module) {
+  $allocatedSidebarModulesByKey[(string)($module['key'] ?? '')] = $module;
+}
+
+$teacherSidebarGroups = [
+  [
+    'label' => 'Academic',
+    'keys' => ['exam_timetable', 'grading_system', 'elearning', 'subject_combinations'],
+  ],
+  [
+    'label' => 'Staff',
+    'keys' => ['roles'],
+  ],
+];
 ?>
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar">
@@ -55,13 +70,29 @@ $allocatedSidebarModules = app_current_user_allocated_portal_modules('teacher');
     <li><a class="app-menu__item<?php echo teacher_menu_active_any(['manage_results', 'results', 'report_card', 'certificates', 'published_analytics', 'print_mark_sheet']); ?>" href="teacher/manage_results"><i class="app-menu__icon feather icon-graph"></i><span class="app-menu__label">Results</span></a></li>
     <li><a class="app-menu__item<?php echo teacher_menu_active('staff_attendance'); ?>" href="teacher/staff_attendance"><i class="app-menu__icon feather icon-clock"></i><span class="app-menu__label">Staff Attendance</span></a></li>
     <li><a class="app-menu__item<?php echo teacher_menu_active('students'); ?>" href="teacher/students"><i class="app-menu__icon feather icon-users"></i><span class="app-menu__label">Students</span></a></li>
-    <?php foreach ($allocatedSidebarModules as $module): ?>
-    <li>
-      <a class="app-menu__item<?php echo teacher_menu_module_active($module); ?>" href="<?php echo htmlspecialchars((string)($module['href'] ?? 'teacher')); ?>">
-        <i class="app-menu__icon <?php echo htmlspecialchars((string)($module['icon'] ?? 'feather icon-grid')); ?>"></i>
-        <span class="app-menu__label"><?php echo htmlspecialchars((string)($module['label'] ?? 'Module')); ?></span>
-      </a>
-    </li>
+    <?php foreach ($teacherSidebarGroups as $group): ?>
+      <?php
+        $groupModules = [];
+        foreach ((array)($group['keys'] ?? []) as $moduleKey) {
+          if (isset($allocatedSidebarModulesByKey[$moduleKey])) {
+            $groupModules[] = $allocatedSidebarModulesByKey[$moduleKey];
+          }
+        }
+        if (!$groupModules) {
+          continue;
+        }
+      ?>
+      <li class="px-3 pt-3 pb-1 text-uppercase" style="font-size:.7rem;letter-spacing:.12em;color:#6f7e8f;font-weight:800;">
+        <?php echo htmlspecialchars((string)$group['label']); ?>
+      </li>
+      <?php foreach ($groupModules as $module): ?>
+      <li>
+        <a class="app-menu__item<?php echo teacher_menu_module_active($module); ?>" href="<?php echo htmlspecialchars((string)($module['href'] ?? 'teacher')); ?>">
+          <i class="app-menu__icon <?php echo htmlspecialchars((string)($module['icon'] ?? 'feather icon-grid')); ?>"></i>
+          <span class="app-menu__label"><?php echo htmlspecialchars((string)($module['label'] ?? 'Module')); ?></span>
+        </a>
+      </li>
+      <?php endforeach; ?>
     <?php endforeach; ?>
     <li><a class="app-menu__item<?php echo teacher_menu_active('how_system_works'); ?>" href="teacher/how_system_works"><i class="app-menu__icon feather icon-help-circle"></i><span class="app-menu__label">How The System Works</span></a></li>
   </ul>
